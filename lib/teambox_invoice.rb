e3b589a0bf5196c85bf5b037c38bffff581d98cb
Priit Tamboom <priit@mx.ee>
2010-07-16 13:34:31 +0200

    included pdfkit middleware

--- lib/teambox_invoice.rb
+++ lib/teambox_invoice.rb
@@ -1,3 +1,4 @@
+require 'pdfkit'
 
 engine = Rails::Plugin.new File.expand_path('../..', __FILE__)
 engine.instance_variable_set('@name', 'teambox_invoice')
@@ -15,3 +16,6 @@ Rails.configuration.eager_load_paths.concat engine.send(:app_paths)
 
 # include sass files to master app
 Sass::Plugin.add_template_location(File.expand_path('../../app/styles', __FILE__))
+
+# include pdfkit middleware
+Rails.configuration.middleware.use PDFKit::Middleware, :page_size => 'A4'
