67406f67de60d017380ab62d02a40774d0938f63
Priit Tamboom <priit@mx.ee>
2010-07-20 11:42:24 +0200

    added todo to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
+  #TODO figure out how to add new routes to master route such as /projects/1/invoices etc
   map.resources :invoices, :member => { :preview => :get }
 end
